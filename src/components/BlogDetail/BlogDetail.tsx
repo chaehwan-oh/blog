1e49c48fa73908b24641cbd61315d366968c8e18
chaehwan-oh <coghks56@gmail.com>
2021-01-19 19:44:50 +0900

    [ADD] blog detail template

---
+++ src/components/BlogDetail/BlogDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BlogDetail() {
+    return (
+        <div className="BlogDetail">
+            리액트 블로그 시작합니다.
+        </div>
+    );
+}
+
+export default BlogDetail;
